37438f9ca517318f0267a750ea2d454e2c3c1fb3
Jschelling0 <101289656+Jschelling0@users.noreply.github.com>
2022-05-06 23:27:48 -1000

    Upload of updated queries sql file

--- queries.sql
+++ queries.sql
@@ -165,3 +165,30 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+SELECT * FROM retirement_info
+SELECT * FROM departments
+SELECT * FROM dept_emp
+-- Query for sales team
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+FROM retirement_info as ri
+	INNER JOIN dept_emp as de
+		ON (ri.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE dept_name in ('Sales')
+
+-- Query for development team
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+FROM retirement_info as ri
+	INNER JOIN dept_emp as de
+		ON (ri.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE dept_name in ('Development')
